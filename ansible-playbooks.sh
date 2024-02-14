76c4ffe4efe1ea72a465b5e13783808ae15fc2dd
Enrique C <>
2024-02-14 15:26:24 -0700

    first sunbeam ansible script

--- ansible-playbooks.sh
+++ ansible-playbooks.sh
@@ -7,58 +7,57 @@ source .env/bin/activate
 
 if [ $# -eq 0 ]; then
     jq -n '{
-        title: "Ansible",
+        title: "Ansible Playbooks",
         description: "Run ansible_fedora playbooks",
         commands: [
             {
-                name: "all",
-                title: "run all roles",
-                mode: "tty",
-                params: [
-                    {
-                        name: "OS",
-                        title: "Linux or MacOs",
-                        type: "string"
-                    }
-                ]
+                name: "ansible-options",
+                title: "Select OS and Roles",
+                mode: "filter"
             },
             {
-                name: "common",
-                title: "common role",
-                mode: "tty",
-                params: [
-                    {
-                        name: "OS",
-                        title: "Linux or MacOS",
-                        type: "string"
-                    }
-                ]
-            },
-            {
-                name: "tools",
-                title: "tools role",
-                mode: "tty",
-                params: [
-                    {
-                        name: "OS",
-                        title: "Linux or MacOS",
-                        type: "string"
-                    }
-                ]
+                name: "run-ansible",
+                title: "run ansible playbooks",
+                mode: "tty"
             }
         ],
     }'
     exit 0
 fi
 
-ITEM=`sunbeam paste`
 COMMAND=$(echo "$1" | jq -r '.command')
-OS=$(echo "$1" | jq -r '.params.OS')
-if [ "$COMMAND" = "tools" ]; then
-    ansible-playbook -i inventories/localhost/localhost playbook/site.yml -t $COMMAND -e OS="$OS"
-elif [ "$COMMAND" = "common" ]; then
-    ansible-playbook -i inventories/localhost/localhost playbook/site.yml -t $COMMAND -K -e OS="$OS"
-elif [ "$COMMAND" = "all" ]; then
-    ansible-playbook -i inventories/localhost/localhost playbook/site.yml -t tools,common -K -e OS="$OS"
+# to be retrieved from the values playbook
+OPTIONS='["linux-all","macos-all","linux-tools","macos-tools","linux-common","macos-common"]'
+
+if [ "$COMMAND" = "ansible-options" ]; then
+    echo $OPTIONS | jq '{
+            "items": map({
+            "title": .,
+            "subtitle": (" ... run "+ (. | split("-")[1]) +  " roles on a " + (. | split("-")[0]) + " localhost"),
+                "actions": [
+                    {
+                        "type": "run",
+                        "title": ("Run Ansible Playbook for " + .),
+                        "command": "run-ansible",
+                        "params": {
+                            "os": . | split("-")[0],
+                            role: . | split("-")[1],
+                        }
+                    }
+                ]
+            })
+        }'
+        exit 0
+fi
+
+if [ "$COMMAND" = "run-ansible" ]; then
+    OS=$(echo "$1" | jq -r '.params.os')
+    ROLE=$(echo "$1" | jq -r '.params.role')
+
+    if [ $ROLE = "all" ]; then
+        ansible-playbook -i inventories/localhost/localhost playbook/site.yml -e OS="$OS"
+    else
+        ansible-playbook -i inventories/localhost/localhost playbook/site.yml -t $ROLE -e OS="$OS"
+    fi
 fi
 
